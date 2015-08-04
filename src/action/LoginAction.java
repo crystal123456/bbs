03a8572dde8b179784bf711795504f0475f02151
crystal123456 <313733041@qq.com>
2015-08-05 06:44:24 +0800

    搭ssh

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -1,5 +1,14 @@
 package action;
 
-public class LoginAction {
+public class LoginAction extends BaseAction{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7922979648150320921L;
+	
+	public String execute(){
+		return SUCCESS;
+	}
 
 }
